16d800359c300eb2fca25f10af6edd8354a5b06e
djairmaykon <djairmaykon@gmail.com>
2022-05-27 23:16:17 -0300

    feat: remove arquivos incorretos

--- 03 - MovieDB/api/src/service/ApiService.ts
+++
@@ -1,9 +0,0 @@
-import { Genre, getGenresParams } from '@controllers/Genre.controller';
-import { getMoviesParams, Movie } from '@controllers/Movie.controller';
-
-export interface ApiService {
-  url: string;
-  apiKey: string;
-  getMovies: (params?: getMoviesParams) => Promise<Movie[]>;
-  getGenres: (params?: getGenresParams) => Promise<Genre[]>;
-}
